c06f519fdbe4fad3f2cc9ec02653cd725af7329b
stefan.prisacariu <stefan.prisacariu@nagarro.com>
2021-06-14 20:15:52 +0300

    Case Study Done

--- src/Department.java
+++ src/Department.java
@@ -1,9 +1,69 @@
+import java.util.Arrays;
+
 public class Department {
     private String name;
-    private Employee[] employees;
+    private Employee[] employees = new Employee[10];
+    private int counter = 0;
 
     public Department(String name) {
         this.name = name;
     }
 
+    public void addEmployee(Employee person) throws Exception {
+        if (counter < 10) {
+            employees[counter] = person;
+            counter++;
+        } else {
+            throw new Exception("Cannot add new employee. Department is full");
+        }
+    }
+
+    public Employee[] getEmployees() {
+        Employee[] nonNullEmployees = new Employee[counter];
+        int index = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                nonNullEmployees[index] = employee;
+                index++;
+            }
+        }
+
+        return nonNullEmployees;
+    }
+
+    public int getNoOfEmployees() {
+        return counter;
+    }
+
+    public Employee getEmployeeByID(int ID) {
+        for (Employee employee : employees) {
+            if (employee.getID() == ID) {
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    public double getTotalSalaries() {
+        double total = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                total = total + employee.getSalary();
+            }
+        }
+        return total;
+    }
+
+    public double getAverageSalary() {
+        return getTotalSalaries() / counter;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "name='" + name + '\'' +
+                ", employees=" + Arrays.toString(employees) +
+                ", counter=" + counter +
+                '}';
+    }
 }
--- src/HRApp.java
+++ src/HRApp.java
@@ -1,2 +1,33 @@
 public class HRApp {
+    public static void main(String[] args) {
+        System.out.println("System Online");
+
+        Department departmentQA = new Department("QA");
+        System.out.println("QA Info: " + departmentQA.toString());
+
+        Employee employee1 = new Employee(12, "Margaret", 98.99);
+        Employee employee2 = new Employee(13, "Daniel", 100.11);
+        Employee employee3 = new Employee(14, "John", 78.11);
+
+        try {
+            departmentQA.addEmployee(employee1);
+            departmentQA.addEmployee(employee2);
+            departmentQA.addEmployee(employee3);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Employee employeeByID = departmentQA.getEmployeeByID(12);
+        System.out.println("Employee Info: " + employeeByID.toString());
+
+
+        for (Employee employeeFromDept : departmentQA.getEmployees()) {
+            System.out.println("Employee Info: " + employeeFromDept.toString());
+        }
+
+        System.out.println("Salaries Total: " + departmentQA.getTotalSalaries());
+
+        System.out.println("Average Salary: " + departmentQA.getAverageSalary());
+
+    }
 }
